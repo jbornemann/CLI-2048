0b5cb939d4d946c6c5d511cd214f3a61510793cf
Jeff Bornemann <bornemannjs@gmail.com>
2014-09-03 22:28:42 -0400

    On going progress

--- src/main/groovy/GameController.groovy
+++ src/main/groovy/GameController.groovy
@@ -1,11 +1,32 @@
+import org.fusesource.jansi.Ansi
+import org.fusesource.jansi.AnsiConsole
 
 class GameController {
 
-    void run(final String[] gameArguments) {
+    private final static LEFT = "s"
+    private final static RIGHT = "f"
+    private final static UP = "e"
+    private final static DOWN = "x"
 
+    void run() {
+        AnsiConsole.systemInstall()
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in)
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader)
+        System.out.println("2048")
+        GameView view = GameView.init()
+        GameEngine engine = GameEngine.init(view)
+        while(engine.running) {
+            def input = bufferedReader.readLine()
+            switch(input) {
+                case LEFT : engine.moveLeft(); break;
+                case RIGHT: engine.moveRight(); break;
+                case UP: engine.moveUp(); break;
+                case DOWN: engine.moveDown(); break;
+            }
+        }
     }
 
     static void main(String[] args) {
-        new GameController(args).run()
+        new GameController().run()
     }
 }
